1f718870899ac7d16d473d2a6aefb2e6d99633a2
GBV <gbvolkov@gmail.com>
2025-11-04 00:53:09 +0300

    feat: Update working prompt to respond in German and change server port to 8084

--- start_openai.ps1
+++ start_openai.ps1
@@ -1,1 +1,1 @@
-uvicorn openai_proxy.main:app --reload --port 8080 --http openai_proxy.http_logging:LoggingH11Protocol
+uvicorn openai_proxy.main:app --reload --port 8084 --http openai_proxy.http_logging:LoggingH11Protocol
