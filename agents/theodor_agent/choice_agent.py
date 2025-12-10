48a344c3d9068a9384f2b20d0ba5b659efc334a7
gbvolkov <gbvolkov@gmail.com>
2025-12-11 01:18:02 +0300

    fallback wit h4.1 base model

--- agents/theodor_agent/choice_agent.py
+++ agents/theodor_agent/choice_agent.py
@@ -76,7 +76,7 @@ _PRIMARY_RETRY_ATTEMPTS = 3
 
 _llm = get_llm(model="base", provider="openai", temperature=1)
 #_llm_fallback = get_llm(model="base", provider="openai", temperature=1)
-_llm_fallback = get_llm(model="base", provider="mistral", temperature=1)
+_llm_fallback = get_llm(model="base", provider="openai_4", temperature=1)
 _llm_fallback_middleware = build_model_fallback_middleware(
     primary_llm=_llm,
     alternative_llm=_llm_fallback,
