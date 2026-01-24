f67861fe7a5651229e57c2e3101ae75d9e6bdd50
gbvolkov <gbvolkov@gmail.com>
2026-01-25 01:48:59 +0300

    feat: Implement Artifact Creator Agent with state management and user interaction prompts

---
+++ agents/artifact_creator_agent/agent.py
@@ -0,0 +1,290 @@
+from __future__ import annotations
+
+import json
+import logging
+import time
+from typing import Annotated, Any, Dict, List, NotRequired, Optional, TypedDict
+
+from langchain.agents import create_agent
+from langchain.agents.middleware import ModelRequest, dynamic_prompt
+from langchain.agents.structured_output import (
+    AutoStrategy,
+    ProviderStrategy,
+    StructuredOutputValidationError,
+    ToolStrategy,
+)
+from langchain_core.language_models import BaseChatModel
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage, ToolMessage
+from langchain_core.messages.modifier import RemoveMessage
+from langchain_core.runnables import RunnableConfig
+
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import END, START, StateGraph
+from langgraph.runtime import Runtime
+from langgraph.types import Command, interrupt
+
+
+from langfuse import Langfuse
+from langfuse.langchain import CallbackHandler
+
+from agents import state
+import config
+
+from utils.utils import is_valid_json_string
+
+from agents.tools.think import ThinkTool
+from agents.tools.yandex_search import YandexSearchTool as SearchTool
+from agents.structured_prompt_utils import provider_then_tool
+from agents.utils import ModelType, get_llm, _extract_text
+
+from platform_utils.llm_logger import JSONFileTracer
+
+from .state import (
+    ArtifactCreatorAgentContext
+    , ArtifactCreatorAgentState
+    , ConfirmationAgentState
+)
+from .utils import UserChangeRequest
+
+
+from .prompts import (
+    GREETINGS_WITH_PROMPT_RU
+    , GREETINGS_WITHOUT_PROMPT_RU
+    , SET_PROPMT_REQUEST_RU
+    , PROMPT_CONFIRMATION_RU
+    , DEFAULT_SYSTEM_PROMPT_RU
+)
+
+LOG = logging.getLogger(__name__)
+
+def route(state: ArtifactCreatorAgentState) -> str:
+    if not state.get("greeted"):
+        return "greetings"
+
+    if not state.get("phase") and not state.get("system_prompt"):
+        return "set_prompt"
+    
+    return state.get("phase")
+
+def create_greetings_node():
+
+    def greetings_node(
+        state: ArtifactCreatorAgentState,
+        config: RunnableConfig,
+        runtime: Runtime[ArtifactCreatorAgentContext],
+    ) -> ArtifactCreatorAgentState:
+        system_prompt = None
+        if runtime.context and runtime.context.get("system_prompt"):
+            system_prompt = runtime.context["system_prompt"]
+            state["system_prompt"] = system_prompt
+            state["phase"] = "cleanup"
+
+        if not state.get("greeted"):
+            greet = (
+                GREETINGS_WITH_PROMPT_RU
+                if state.get("system_prompt")
+                else GREETINGS_WITHOUT_PROMPT_RU
+            )
+            state["messages"] = (state.get("messages") or []) + [AIMessage(content=greet)]
+            state["greeted"] = True
+        return state
+
+    return greetings_node
+
+
+def create_set_prompt_node(model: BaseChatModel):
+
+    def set_prompt_node(
+        state: ArtifactCreatorAgentState,
+        config: RunnableConfig,
+        runtime: Runtime[ArtifactCreatorAgentContext],
+    ) -> ArtifactCreatorAgentState:
+        #state["phase"] = "run"
+        
+        original_messages = state.get("messages", [])
+
+        for msg in reversed(original_messages):
+            if isinstance(msg, HumanMessage) or getattr(msg, "type", "") == "human":
+                last_user_msg = msg
+                break
+        #system_prompt = None
+        if last_user_msg is None:
+            response_text = SET_PROPMT_REQUEST_RU
+            state["phase"] = "set_prompt"
+            return state
+        else:
+            response_text = PROMPT_CONFIRMATION_RU
+            #all_msg_ids = [m.id for m in state["messages"]]
+            state["system_prompt"] = last_user_msg.content
+            state["phase"] = "cleanup"
+            # Returning RemoveMessage instances instructs the reducer to delete them
+            #return {
+            #    "messages": [RemoveMessage(id=mid) for mid in all_msg_ids] + [AIMessage(content=response_text)],
+            #    "system_prompt": system_prompt,
+            #    "phase" : "cleanup"
+            #}            
+        state["messages"].append(AIMessage(content=response_text))
+        return state
+            
+
+    return set_prompt_node
+
+
+def cleanup_messages_node(
+        state: ArtifactCreatorAgentState,
+        config: RunnableConfig,
+        runtime: Runtime[ArtifactCreatorAgentContext],
+    ) -> ArtifactCreatorAgentState:
+    
+    all_msg_ids = [m.id for m in state["messages"][:-1]]
+    # Returning RemoveMessage instances instructs the reducer to delete them
+    return {
+        "messages": [RemoveMessage(id=mid) for mid in all_msg_ids] + [state["messages"][-1]],
+        "phase" : "run"
+    }
+
+def _build_run_agent(model: BaseChatModel):
+    @dynamic_prompt
+    def build_prompt(request: ModelRequest) -> str:
+        state: ArtifactCreatorAgentState = request.state
+        system_prompt = state.get("system_prompt")
+        return system_prompt or DEFAULT_SYSTEM_PROMPT_RU
+
+    return create_agent(
+        model=model,
+        #tools=[_think_tool],
+        middleware=[build_prompt],
+        state_schema=ArtifactCreatorAgentState,
+        context_schema=ArtifactCreatorAgentContext,
+    )
+    
+def _build_confirmation_agent(model: BaseChatModel):
+
+    @dynamic_prompt
+    def build_prompt(request: ModelRequest) -> str:
+        state: ConfirmationAgentState  = request.state
+        #response = state.get("last_user_answer")
+        prompt = (
+            "You have to analyze user's response.\n" 
+            "Determine if user confirmed the text or requested change."        
+            #f"User response is: {response}\n"
+        )
+        return prompt
+
+    return create_agent(
+        model=model,
+        middleware=[provider_then_tool],
+        system_prompt = "You have to analyze user's response.\nDetermine if user confirmed the text or requested change.",
+        response_format=UserChangeRequest,
+        state_schema=ConfirmationAgentState,
+    )
+
+def create_confirmation_node(model: BaseChatModel):
+    _confirmation_agent = _build_confirmation_agent(model)
+    def confirmation_node(
+        state: ArtifactCreatorAgentState, 
+        config: RunnableConfig, 
+        runtime: Runtime[ArtifactCreatorAgentContext],
+    ) -> ArtifactCreatorAgentState:
+        interrupt_payload = {
+            "type": "confirm",
+            "content": state["artifact"],
+            "question": "Подтвердите контекст или предложите изменения.",
+        }
+        user_response = interrupt(interrupt_payload)
+        message_update = [HumanMessage(content=str(user_response))]    
+        state["last_user_answer"] = user_response
+
+        confirmation_state = {
+            "messages": message_update,  # ensure no chat history is provided
+        }
+
+        result = _confirmation_agent.invoke(confirmation_state, config=config, context=runtime.context)
+        structured = result.get("structured_response") or {}
+        if structured.is_change_requested:
+            return Command(
+                goto="run",
+                update={
+                    "messages": message_update,
+                },
+            )
+        return Command(
+            update={
+                "messages": message_update,
+            },
+        )
+
+    return confirmation_node    
+
+
+    
+def create_run_node(model: BaseChatModel):
+    _run_agent = _build_run_agent(model)
+    def run_node(
+        state: ArtifactCreatorAgentState,
+        config: RunnableConfig,
+        runtime: Runtime[ArtifactCreatorAgentContext],
+    ) -> ArtifactCreatorAgentState:
+        
+        result_state = _run_agent.invoke(state, config=config, context=runtime.context)
+        last_message = result_state["messages"][-1]
+        if isinstance(last_message, AIMessage):
+            result_state["artifact"] = _extract_text(last_message)
+
+        return result_state
+
+    return run_node
+
+
+def initialize_agent(
+    provider: ModelType = ModelType.GPT,
+    use_platform_store: bool = False,
+    locale: str = "en",
+    checkpoint_saver=None,
+):
+    #set_locale(locale)
+    #set_models_locale(locale)
+    log_name = f"artifact_creator_agent_{time.strftime('%Y%m%d%H%M')}"
+    json_handler = JSONFileTracer(f"./logs/{log_name}")
+    callback_handlers = [json_handler]
+    if config.LANGFUSE_URL and len(config.LANGFUSE_URL) > 0:
+        langfuse = Langfuse(
+            public_key=config.LANGFUSE_PUBLIC,
+            secret_key=config.LANGFUSE_SECRET,
+            host=config.LANGFUSE_URL,
+        )
+        lf_handler = CallbackHandler()
+        callback_handlers += [lf_handler]
+
+    memory = None if use_platform_store else checkpoint_saver or MemorySaver()
+    llm = get_llm(model="base", provider=provider.value, temperature=0.4)
+    response_analyser_llm = get_llm(model="nano", provider=provider.value, temperature=0)
+    
+    builder = StateGraph(ArtifactCreatorAgentState)
+    builder.add_node("greetings", create_greetings_node())
+    builder.add_node("set_prompt", create_set_prompt_node(llm))
+    builder.add_node("cleanup", cleanup_messages_node)
+    builder.add_node("run", create_run_node(llm))
+    try:
+        builder.add_node("confirm", create_confirmation_node(response_analyser_llm))
+    except Exception as e:
+        print(e)
+
+    builder.add_conditional_edges(
+        START,
+        route,
+        {
+            "greetings": "greetings",
+            "set_prompt": "set_prompt",
+            "cleanup": "cleanup",
+            "run": "run"
+        },
+    )
+    builder.add_edge("greetings", END)
+    builder.add_edge("set_prompt", END)
+    builder.add_edge("cleanup", "run")
+    builder.add_edge("run", "confirm")
+    builder.add_edge("confirm", END)
+
+    graph = builder.compile(checkpointer=memory, debug=False).with_config({"callbacks": callback_handlers})
+    return graph
---
+++ agents/artifact_creator_agent/state.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+from typing import Annotated, Any, Dict, List, NotRequired, Optional, TypedDict
+
+from langchain.agents import AgentState
+from langchain_core.messages import BaseMessage
+from langgraph.graph.message import add_messages
+
+
+class ArtifactCreatorAgentContext(TypedDict, total=False):
+    """Runtime context for the system agent."""
+
+    system_prompt: str
+
+
+class ArtifactCreatorAgentState(AgentState[Dict[str, Any]]):
+    """State schema for simple agent graph."""
+
+    messages: Annotated[List[BaseMessage], add_messages]
+    attachments: NotRequired[List[Dict[str, Any]]]
+
+    system_prompt: NotRequired[str]
+
+    # phases: init -> set_prompt -> run
+    phase: NotRequired[str]
+    greeted: NotRequired[bool]
+    artifact: NotRequired[str]
+    last_user_answer: NotRequired[str]
+    is_user_confirmed: NotRequired[bool]
+
+class ConfirmationAgentState(TypedDict, total=False):
+    # Keep it minimal. Only what the confirmation agent needs.
+    messages: Annotated[List[BaseMessage], add_messages]
+    last_user_answer: str
